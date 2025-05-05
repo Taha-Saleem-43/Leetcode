010ec865eeda2dd4e42bd065d15c849d60be3a88
Taha Saleem <tahasaleem981@gmail.com>
2025-05-06 00:54:38 +0500

    Time: 2 ms (5.02%), Space: 8.7 MB (17.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int prev1=1;
-        int prev2=1;
-        for(int i=2;i<=n;i++){
-            int curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        vector<int> dp(n+1,-1);
+        return recursion(n,dp);
     }
-
+    int recursion(int n,vector<int>& dp){
+        if (n==1 || n==0) return 1;
+        if (dp[n]!=-1) return dp[n];
+        return dp[n] = recursion(n-1,dp) + recursion(n-2,dp);
+    }
 };
