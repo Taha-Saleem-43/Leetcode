d4d05755b07168f9554086e655accb1a78e623de
Taha Saleem <tahasaleem981@gmail.com>
2025-07-07 23:45:07 +0500

    Time: 67 ms (38.92%), Space: 74.8 MB (98.35%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -2,27 +2,43 @@ class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
         int n = events.size();
+
+        // Find the last day of any event (max end day)
         int maxDay = 0;
-        for (int i = 0; i < events.size(); i++) {
+        for (int i = 0; i < n; i++) {
             maxDay = max(maxDay, events[i][1]);
         }
-        priority_queue<int, vector<int>, greater<>> pq;
+
+        // Min-heap to store end days of available events
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        // Sort events based on start day
         sort(events.begin(), events.end());
-        int ans = 0;
-        for (int i = 0, j = 0; i <= maxDay; i++) {
-            while (j < n && events[j][0] <= i) {
-                pq.emplace(events[j][1]);
-                j++;
+
+        int count = 0;  // Total events attended
+        int i = 0;      // Pointer for events list
+
+        // Loop through each day
+        for (int day = 1; day <= maxDay; day++) {
+
+            // Add all events that start today or earlier
+            while (i < n && events[i][0] <= day) {
+                minHeap.push(events[i][1]); // Add end day of the event
+                i++;
             }
-            while (!pq.empty() && pq.top() < i) {
-                pq.pop();
+
+            // Remove events that already expired (end day before today)
+            while (!minHeap.empty() && minHeap.top() < day) {
+                minHeap.pop();
             }
-            if (!pq.empty()) {
-                pq.pop();
-                ans++;
+
+            // Attend the event with the earliest end day (greedy choice)
+            if (!minHeap.empty()) {
+                minHeap.pop();  // Attend one event today
+                count++;
             }
         }
 
-        return ans;
+        return count; // Maximum events we can attend
     }
 };
